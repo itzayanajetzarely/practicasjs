75ba800d90688599965ad8bcbf9c7152e35e2f1a
madison <madison>
2023-05-11 08:15:11 -0500

    first commit

---
+++ 01 hola/main.js
@@ -0,0 +1,11 @@
+const hola=()=>{
+    alert("hola mundo");
+    let nombre=prompt("cual es tu nombre?");
+    alert("hola "+nombre+" ")
+    }
+    const suma=()=>{
+    let numero1=parseInt (prompt("numero1:"));
+        let numero2=parseInt (prompt("numero2:"));
+        let suma= numero1+numero2;
+        alert("suma:"+suma);
+    }
